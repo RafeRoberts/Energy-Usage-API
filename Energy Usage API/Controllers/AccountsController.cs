a8024bfe73217ee8f3045d3ea3071d71976e0896
mrrafe <rafe@pixelcase.com>
2021-04-11 18:52:48 +0800

    Added Accounts

---
+++ Energy Usage API/Controllers/AccountsController.cs
@@ -0,0 +1,56 @@
+﻿using Energy_Usage_API.Reposities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Energy_Usage_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+       
+        private readonly InMemoryAccountsRepository repository;
+
+        public AccountsController()
+        {
+            repository = new InMemoryAccountsRepository();
+        }
+        // GET: api/<AccountsController>
+        [HttpGet]
+        public IEnumerable<Models.Account> GetAccounts()
+        {
+            var accounts = repository.GetAccounts();
+            return accounts;
+        }
+
+        // GET api/<AccountsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Energy Usage API/Repositories/InMemoryAccountsRepository.cs
@@ -0,0 +1,30 @@
+﻿using Energy_Usage_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Energy_Usage_API.Reposities
+{
+
+    public class InMemoryAccountsRepository
+    {
+
+
+        private readonly List<Account> accounts = new()
+        {
+            new Account { Id = Guid.NewGuid(), AccountId = 2344, FirstName = "Tommy", LastName = "Test" },
+            new Account { Id = Guid.NewGuid(), AccountId = 2233, FirstName = "Barry", LastName = "Test" },
+            new Account { Id = Guid.NewGuid(), AccountId = 8766, FirstName = "Sally", LastName = "Test" }
+        };
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return accounts;
+        }
+
+        public Account GetAccount(Guid id)
+        {
+            return accounts.Where(account => account.Id == id).SingleOrDefault();
+        }
+    }
+}
